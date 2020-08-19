583860146a570d54bac8e98c185090e01b49f83b
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-08-19 22:23:56 +0700

    feature/report

--- api/controllers/report.controller.js
+++ api/controllers/report.controller.js
@@ -1,13 +1,21 @@
-const reportModel = require('../../models/report.model'); // Khai báo cấu hình Model
+const reportModel = require("../../models/report.model"); // Khai báo cấu hình Model
 
-module.exports.index = async (req, res, next) => { 
-	// Hien thi tat ca database duoi dang json
-  var reports = await reportModel.find();
+module.exports.index = async (req, res, next) => {
+  // Hien thi tat ca database duoi dang json
+  let reports;
+  if (req.query.m && req.query.y) {
+    reports = await reportModel.find({
+      month: req.query.m,
+      year: req.query.y,
+    });
+  } else {
+    reports = await reportModel.find();
+  }
   res.json(reports);
-}
+};
 
 module.exports.create = async (req, res, next) => {
-	// Them moi vao database.
+  // Them moi vao database.
   var reports = await reportModel.create(req.body);
   res.json(reports);
-}
+};
--- models/report.model.js
+++ models/report.model.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 var reportSchema = new mongoose.Schema({
-  groupId: String,
   month: Number,
   year: Number,
   beginningBalance: Number,
   endingBalance: Number,
   usedMoney: Number,
-  earnedMoney: Number
+  earnedMoney: Number,
+  listTransaction: Object
 })
 
 module.exports = mongoose.model('reports', reportSchema)
