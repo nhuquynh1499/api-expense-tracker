8627eba5db75a2cfe70d64e91b6189d1ba00f5ef
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-09-23 14:21:59 +0700

    RestAPI and logic Signin/Signup

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const apiGroupRoute = require('./api/routes/group.route');
 const apiTransactionRoute = require('./api/routes/transaction.route');
 const apiPlanRoute = require('./api/routes/plan.route');
 const apiReportRoute = require('./api/routes/report.route');
+const apiUserRoute = require('./api/routes/user.route');
+const apiLoginRoute = require('./api/routes/login.route');
 
 connect().then(() => {
   console.log('Connect success');
@@ -24,6 +26,8 @@ app.use('/api/group', apiGroupRoute);
 app.use('/api/transaction', apiTransactionRoute);
 app.use('/api/planning', apiPlanRoute);
 app.use('/api/report', apiReportRoute);
+app.use('/api/user', apiUserRoute);
+app.use('/api/login', apiLoginRoute);
 
 app.listen(port, () => 
 	console.log(`Example app listening at http://localhost:${port}`)
