445b94484890acadecc56cd81a325ab5809fe9ed
Nhu Quynh <ngngocnhuquynh0104@gmail.com>
2020-09-29 15:51:12 +0700

    Delete user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,20 @@
 const userModel = require('../models/user.model');
+const planModel = require('../models/plan.model');
+const transactionModel = require('../models/transaction.model');
+const reportModel = require('../models/report.model');
 
 module.exports.index = async (req, res) => {
   var users = await userModel.find();
   res.render('./users/index', {
     users: users
   });
+}
+
+module.exports.delete = async (req,res) => {
+  const userId = req.params.id
+  await userModel.remove({ _id: userId }).exec();
+  await planModel.remove({ userId: userId }).exec();
+  await transactionModel.remove({ userId: userId }).exec();
+  await reportModel.remove({ userId: userId }).exec();
+  res.redirect("/user");
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,7 +5,7 @@ var controller = require('../controllers/user.controller');
 
 router.get('/', controller.index);
 
-// router.get('/:id', controller.index);
+router.get('/:id/delete', controller.delete);
 
 // router.post('/', controller.create);
 
